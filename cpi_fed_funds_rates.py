4d71bd445eb62304c57c411dcd2b40c7f6cc9d61
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-08-19 18:32:05 -0400

    Updates for notebook

--- cpi_fed_funds_rates.py
+++ cpi_fed_funds_rates.py
@@ -179,3 +179,156 @@ ax2.xaxis.set_major_locator(mtick.NullLocator())
 _ = ax.set_xticklabels(list(map("{:%Y-%m}".format, df.index[::4])))
 
 # %%
+
+df = df.loc[: real_gdp.last_valid_index()]  # Truncate based on last valid GDP value
+print(
+    "Gap between intrinsic (GDP+CPI) and actual (10y Treasury) riskfree rates in quarter starting in {:%Y-%m}: {:.2%}\n"
+    "Compared with: {:.2%} one quarter earlier\n".format(
+        df.index[-1].date(),
+        df.iloc[-1, 0] + df.iloc[-1, 1] - df.iloc[-1, 3],
+        df.iloc[-2, 0] + df.iloc[-2, 1] - df.iloc[-2, 3],
+    )
+)
+
+print(
+    "Gap between intrinsic (GDP+CPI) and actual (3m Treasury) riskfree rates in quarter starting in {:%Y-%m}: {:.2%}\n"
+    "Compared with: {:.2%} one quarter earlier\n".format(
+        df.index[-1].date(),
+        df.iloc[-1, 0] + df.iloc[-1, 1] - df.iloc[-1, 4],
+        df.iloc[-2, 0] + df.iloc[-2, 1] - df.iloc[-2, 4],
+    )
+)
+
+from_dt = date(1997, 1, 1)
+# from_dt = df.index[0]
+means = df.loc[from_dt:].mean()
+
+print(
+    "Average gap between intrinsic (GDP+CPI) and actual (10y Treasury) riskfree rates starting from {:%Y-%m}: {:.2%}".format(
+        from_dt, means.iloc[0] + means.iloc[1] - means.iloc[3]
+    )
+)
+print(
+    "Average gap between intrinsic (GDP+CPI) and actual (3m Treasury) riskfree rates starting from {:%Y-%m}: {:.2%}".format(
+        from_dt, means.iloc[0] + means.iloc[1] - means.iloc[4]
+    )
+)
+# %%
+
+df_gdp_tips = df.iloc[:, [0, -1]]
+df_gdp_tips = df_gdp_tips.loc[df_gdp_tips.iloc[:, -1].first_valid_index() :]
+ax = df_gdp_tips.iloc[:, -1].plot(
+    figsize=(20, 10),
+    grid=True,
+    title="Real GDP vs 10y Real Yield (TIPS)",
+    linewidth=2,
+    xticks=df_gdp_tips.index[::2],
+    yticks=np.arange(-0.075, 0.2, 0.025),
+    xlabel="",
+    rot=45,
+    linestyle="--",
+    color="k",
+)
+ax2 = ax.twiny()
+df_gdp_tips.iloc[:, 0].plot.bar(ax=ax2, xlabel="", stacked=True)
+ax.legend(loc="upper left")
+ax2.legend(loc="upper right")
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
+ax2.xaxis.set_major_locator(mtick.NullLocator())
+_ = ax.set_xticklabels(list(map("{:%Y-%m}".format, df_gdp_tips.index[::2])))
+
+
+# %%
+
+df_gdp_tips = df_gdp_tips.loc[: real_gdp.last_valid_index()]
+print(
+    "Gap between intrinsic GDP and actual 10y Real Riskfree Rate in quarter starting in {:%Y-%m}: {:.2%}\n"
+    "Compared with: {:.2%} one quarter earlier\n".format(
+        df_gdp_tips.index[-1].date(),
+        df_gdp_tips.iloc[-1, 0] - df.iloc[-1, -1],
+        df.iloc[-2, 0] - df.iloc[-2, -1],
+    )
+)
+# %%
+
+report_start = date.fromisoformat("2012-01-01")
+
+# Treasury yield curve
+data = web.get_data_fred(
+    ["FEDFUNDS", "GS1", "GS2", "GS3", "GS5", "GS7", "GS10", "GS20", "GS30"],
+    report_start,
+    report_end,
+)
+data.dropna(inplace=True)
+
+# The following data series aare only provided with daily frequences, hence we need to downsample them
+# by taking their mean value over a given month and rounding to two decimal points (that's how values
+# in series with monthly frequencies aaare calculated)
+data2 = web.get_data_fred(["DGS1MO", "DGS3MO", "DGS6MO"], report_start, report_end)
+data2 = data2.resample("MS").mean().round(2)
+
+data = pd.concat([data, data2], axis=1).dropna()
+
+# Rearranging columns
+data = data[
+    [
+        "FEDFUNDS",
+        "DGS1MO",
+        "DGS3MO",
+        "DGS6MO",
+        "FEDFUNDS",
+        "GS1",
+        "GS2",
+        "GS3",
+        "GS5",
+        "GS7",
+        "GS10",
+        "GS30",
+    ]
+]
+
+# Renaming to reflect quarterly frequencies in the columns that used to have monthly frequencies
+data.rename(
+    columns={"DGS1MO": "GS1MO", "DGS3MO": "GS3MO", "DGS6MO": "GS6MO"}, inplace=True
+)
+
+data /= 100.0
+
+# Converting the Fed Funds Rate to actual/actual
+leap_year_cond = data.FEDFUNDS.index.year % 4 == 0 & (
+    (data.FEDFUNDS.index.year % 100 != 0) | (data.FEDFUNDS.index.year % 400 == 0)
+)
+data.FEDFUNDS[leap_year_cond] *= 366.0 / 360
+data.FEDFUNDS[np.invert(leap_year_cond)] *= 365.0 / 360
+
+# Convertinng all CMT Yields to APY
+data.iloc[:, 1:] = (data.iloc[:, 1:] / 2 + 1) ** 2 - 1
+
+data
+
+# %%
+
+# Inflation expectation as givenn by breakeven inflation rates
+data_infl_brk_even = web.get_data_fred(
+    ["T5YIEM", "T7YIEM", "T10YIEM", "T20YIEM", "T30YIEM"], report_start, report_end
+)
+data_infl_brk_even.dropna(inplace=True)
+
+cpi = fred_cpi_ffr[["Annual seasonaly adjusted CPI"]].loc["2012":]
+
+# Expected inflation as calculated by the Federal Reserve Bank of Cleveland based on Inflation swap data,
+# Treasury Yields, current CPI, Blue Chip forecast of CPI
+data_infl = web.get_data_fred(
+    ["EXPINF" + str(i) + "YR" for i in range(1, 31)], report_start, report_end
+).shift(-1)
+
+# Converting to decimal fractions, i.e. 1% is 0.01
+data_infl_brk_even /= 100.0
+data_infl /= 100.0
+
+# Adding CPI and The Federal Reserve Bank of Cleveland 1 Year Inflation Expectation
+data_infl = pd.concat([cpi, data_infl], axis=1).dropna()
+data_infl_brk_even = pd.concat([cpi, data_infl_brk_even], axis=1)
+
+# data_infl_brk_even.fillna(method='ffill', inplace=True
+data_infl_brk_even
